bf67f63c6fb5386ba0c1d1b6fbc6f5042475302a
Paolo Roselli <paolo.roselli02@gmail.com>
2024-05-20 17:37:48 +0200

    add url for smartphone

--- OtpClient/main.py
+++ OtpClient/main.py
@@ -5,6 +5,8 @@ import time
 secret_base32 = 'thisisoursecretkey'
 
 totp = pyotp.TOTP(secret_base32)
+url = pyotp.totp.TOTP(secret_base32).provisioning_uri(name='test@account.it', issuer_name='Esame Architetture')
+print(url)
 while True:
     current_value = totp.now() # => '492039'
 
