fb67c190659d1042ecc1d7521fe809233c0cd977
Marco <you@example.com>
2024-05-24 18:26:38 +0200

    Arduino Read Serial

--- Arduino/comunicazioneSerialeDefinitiva.ino
+++ Arduino/comunicazioneSerialeDefinitiva.ino
@@ -9,6 +9,8 @@ const int ledPin = 11;
 
 bool distanceReached=false;
 float duration, distance; 
+int stato = 0;
+char c="";
 
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
@@ -18,6 +20,7 @@ char hexaKeys[ROWS][COLS] = {
 };
 
 String strToSend="";
+String strReceived="";
 
 byte rowPins[ROWS] = {8, 7, 6, 5}; // row pinouts of the keypad R1 = D8, R2 = D7, R3 = D6, R4 = D5
 byte colPins[COLS] = {4, 13, 12, 2};    // column pinouts of the keypad C1 = D4, C2 = D3, C3 = D2
@@ -33,32 +36,49 @@ void setup()
  
 void loop()
 {
-  digitalWrite(trigPin, LOW);  
-	delayMicroseconds(2);  
-	digitalWrite(trigPin, HIGH);  
-	delayMicroseconds(10);  
-	digitalWrite(trigPin, LOW); 
-  duration = pulseIn(echoPin, HIGH);  
-  distance = (duration*.0343)/2;
-  delay(100);  
-  if(distance < 10)
-  {
-    distanceReached = true;
-    digitalWrite(ledPin,HIGH);
-    Serial.println("camera");
+  if(stato == 0){
+    digitalWrite(trigPin, LOW);  
+    delayMicroseconds(2);  
+    digitalWrite(trigPin, HIGH);  
+    delayMicroseconds(10);  
+    digitalWrite(trigPin, LOW); 
+    duration = pulseIn(echoPin, HIGH);  
+    distance = (duration*.0343)/2;
+    delay(100);  
+    if(distance < 10)
+    {
+      digitalWrite(ledPin,HIGH);
+      Serial.println("A");
+      stato = 1;
+    }
   }
-  while(distanceReached)
-  {
+  else if(stato == 1){
     char customKey = customKeypad.getKey();
-  
     if (customKey){
       if(customKey!='*')
       {
         strToSend.concat(customKey);
       }
       else   
+      {
         Serial.println(strToSend);
+        stato = 2;
+        Serial.println(stato);
+      }
     }
   }
-  
+  else if(stato == 2){
+    if(Serial.available()) {
+      do {
+        if(Serial.available()) {
+          c = Serial.read();
+          strReceived+= c;
+        }
+      }while(c != '\n');
+      Serial.println(strReceived);
+    }
+    if(strReceived == "ciao"){
+      Serial.println("ok");
+    }
+  }
 }
