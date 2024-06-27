e1642bf9235f2bf2c176c4db0affb28f5a0a4ec2
Marco <you@example.com>
2024-06-27 19:59:34 +0200

    script fswebcam

--- Raspberry/capture_images.py
+++ Raspberry/capture_images.py
@@ -1,8 +1,23 @@
-from picamera import PiCamera
+#from picamera import PiCamera
 from time import sleep
+import subprocess
+from datetime import datetime
+import os
 
-camera = PiCamera()
+#camera = PiCamera()
 
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+#camera.start_preview()
+#sleep(5)
+#camera.stop_preview()
+
+lapse_dir = datetime.strftime(datetime.now(),"timelapse_%Y%m%d-%H%M%S")
+subprocess.call(['mkdir',lapse_dir])
+
+prefix = datetime.strftime(datetime.now(),"%Y%m%d-%H%M%S")
+filename = lapse_dir+"/"+prefix+".jpg"
+
+subprocess.call(['fswebcam','-r','350x350','--no-banner',filename])
+
+print("Saving photo to %s"%(filename))
+
+#time.sleep(10)
